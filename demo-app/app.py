c4f23c35628e450d0c253deec8cf8617333d9f81
Sarthak074 <bhandaresarthak074@gmail.com>
2026-01-29 18:03:39 +0530

    Initial commit

---
+++ demo-app/app.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+import time
+import threading
+
+app=FastAPI()
+
+memory_holder=[]
+
+@app.get("/")
+def root():
+    return {"status":"Demo app running successfully"}
+
+@app.get("/cpu") #For CPU loads
+def cpu_load():
+    def burn_cpu():
+        end_time=time.time()+10
+
+        while time.time()<end_time:
+            pass
+    
+    thread=threading.Thread(target=burn_cpu)
+    thread.start()
+    return{"message":"CPU load started for 10 seconds"}
+
+@app.get("/memory") #Memory load
+def memory_load():
+    for _ in range(10):
+        memory_holder.append("X" * 10**6) # ~10mb
+    return {"message":"Memory usage increased"}
+
+@app.get("/latency") #Response
+def latency():
+    time.sleep(2)
+    return {"message":"Response deplayed by 2 seconds"}
